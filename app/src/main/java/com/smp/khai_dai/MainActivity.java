c28d4b9952b92fdf791296eb1d685f5a5b13afe3
Syeda-Mahjabin-Proma <syedamahjabinproma@gmail.com>
2024-03-12 21:05:08 +0600

    login_xml done

--- app/src/main/java/com/smp/khai_dai/MainActivity.java
+++ app/src/main/java/com/smp/khai_dai/MainActivity.java
@@ -2,13 +2,43 @@ package com.smp.khai_dai;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    private Button loginButton;
+    private Button signupButton;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        findViews();
+        onClick();
+    }
+
+    private void findViews() {
+        loginButton = findViewById(R.id.login);
+        signupButton = findViewById(R.id.signup);
+    }
+
+    private void onClick() {
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, login.class));
+            }
+        });
+
+        signupButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, signup.class));
+            }
+        });
+
     }
 }
---
+++ app/src/main/java/com/smp/khai_dai/login.java
@@ -0,0 +1,13 @@
+package com.smp.khai_dai;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class login extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.login);
+    }
+}
